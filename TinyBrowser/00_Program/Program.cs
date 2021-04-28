201b9c37ea0c1ee6b5b705808e0068345852208e
Cristel Capetillo <cristel.capetillo@outlook.com>
2021-04-28 14:55:23 +0200

    This does something work

--- TinyBrowser/00_Program/Program.cs
+++ TinyBrowser/00_Program/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net.Sockets;
 using System.Text;
 
-namespace TinyBrowser {
-    
-    class Program {
-        static void Main(string[] args) {
-            var tcpClient = new TcpClient("acme.com", 80);
-            var stream = tcpClient.GetStream();
-            var bytes = Encoding.ASCII.GetBytes("GET / HTTP/1.1\r\nHost: acme.com\r\n\r\n");
-            stream.Write(bytes);
+public static class Program {
+    static void Main(string[] arguments) {
+        TcpClient tcpClient = new TcpClient();
+        tcpClient.Connect("www.acme.com", 80);
+
+        using (NetworkStream networkStream = tcpClient.GetStream()) {
+            StreamWriter streamWriter = new StreamWriter(networkStream);
+            StreamReader streamReader = new StreamReader(networkStream);
+
+            string requestHttp = "";
+            requestHttp += "GET / HTTP/1.0\r\n";
+            requestHttp += "Host: www.acme.com\r\n";
+            requestHttp += "\r\n";
+
+            streamWriter.Write(requestHttp);
+            streamWriter.Flush();
+
+            Console.WriteLine("[Reading website...]");
+            Console.WriteLine(streamReader.ReadToEnd());
         }
+
+        tcpClient.Close();
+        Console.WriteLine("[Done reading website!]");
+        Console.ReadKey();
     }
 }
+
