6dd6518159475a9ff45b76fcc658948529a35c21
Cristel Capetillo <cristel.capetillo@outlook.com>
2021-04-28 15:55:46 +0200

    Adds more structure

--- TinyBrowser/00_Program/Program.cs
+++ TinyBrowser/00_Program/Program.cs
@@ -1,31 +1,17 @@
 ﻿using System;
 using System.IO;
 using System.Net.Sockets;
+using TinyBrowser._01_Browser;
 
 
-public static class Program {
+class Program {
+    
     static void Main(string[] arguments) {
-        TcpClient tcpClient = new TcpClient();
-        tcpClient.Connect("www.acme.com", 80);
-
-        using (NetworkStream networkStream = tcpClient.GetStream()) {
-            StreamWriter streamWriter = new StreamWriter(networkStream);
-            StreamReader streamReader = new StreamReader(networkStream);
-
-            string requestHttp = "";
-            requestHttp += "GET / HTTP/1.1\r\n";
-            requestHttp += "Host: www.acme.com\r\n\r\n";
-            
-            streamWriter.Write(requestHttp);
-            streamWriter.Flush();
-
-            Console.WriteLine("Reading website...");
-            Console.WriteLine(streamReader.ReadToEnd());
-        }
-
-        tcpClient.Close();
-        Console.WriteLine("Done reading website\nPress any key to exit");
-        Console.ReadKey();
+        Browser browser = new Browser();
+        
+        browser.ClientConnect();
+        browser.ReadWebsite();
+        browser.StopReadingWebsite();
     }
 }
 
---
+++ TinyBrowser/01_Browser/Browser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace TinyBrowser._01_Browser {
+    public class Browser {
+        TcpClient tcpClient = new TcpClient();
+        string hostName = "www.acme.com";
+        int port = 80;
+
+        public void ClientConnect() {
+            tcpClient.Connect(hostName, port);
+        }
+        
+        public void ReadWebsite() {
+            var networkStream = tcpClient.GetStream();
+            var streamWriter = new StreamWriter(networkStream);
+            var streamReader = new StreamReader(networkStream);
+            
+            string requestHttp = "";
+            requestHttp += "GET / HTTP/1.1\r\n";
+            requestHttp += "Host: www.acme.com\r\n\r\n";
+            
+            streamWriter.Write(requestHttp);
+            streamWriter.Flush();
+
+            Console.WriteLine("Reading website...");
+            Console.WriteLine(streamReader.ReadToEnd());
+        }
+    
+    
+        public void StopReadingWebsite() {
+            tcpClient.Close();
+            Console.WriteLine("Done reading website\nPress any key to exit");
+            Console.ReadKey();
+        }
+
+    }
+}
