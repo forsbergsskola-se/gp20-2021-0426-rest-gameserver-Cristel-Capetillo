cef2d25f1cbdf3b2894d40997fe10e082da60637
Cristel Capetillo <cristel.capetillo@outlook.com>
2021-04-28 13:33:34 +0200

    Adds sample code

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Text;
 
 namespace TinyBrowser {
     
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var tcpClient = new TcpClient("acme.com", 80);
+            var stream = tcpClient.GetStream();
+            var bytes = Encoding.ASCII.GetBytes("GET / HTTP/1.1\r\nHost: acme.com\r\n\r\n");
+            stream.Write(bytes);
         }
     }
 }
