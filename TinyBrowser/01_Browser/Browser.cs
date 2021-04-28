972def366faee241da2ad6bc9c697c0d43b7b7b2
Cristel Capetillo <cristel.capetillo@outlook.com>
2021-04-28 17:02:17 +0200

    Applies Rider suggestions

--- TinyBrowser/01_Browser/Browser.cs
+++ TinyBrowser/01_Browser/Browser.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 
 namespace TinyBrowser._01_Browser {
     public class Browser {
-        TcpClient tcpClient = new TcpClient();
+        readonly TcpClient tcpClient = new TcpClient();
         string hostName = "www.acme.com";
         int port = 80;
 
